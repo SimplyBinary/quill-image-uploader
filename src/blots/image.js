042da0fccb3a4e9e32aa83f000a178f4469591b1
csculley <csculley@csculley.com>
2024-08-27 10:03:35 -0400

    format with prettier

--- src/blots/image.js
+++ src/blots/image.js
@@ -3,15 +3,15 @@ import Quill from "quill";
 const Block = Quill.import("blots/block");
 
 class LoadingImage extends Block {
-    static create(src) {
-        const node = super.create(src);
-        if (src === true) return node;
+  static create(src) {
+    const node = super.create(src);
+    if (src === true) return node;
 
-        const image = document.createElement("img");
-        image.setAttribute("src", src);
-        node.appendChild(image);
-        return node;
-    }
+    const image = document.createElement("img");
+    image.setAttribute("src", src);
+    node.appendChild(image);
+    return node;
+  }
 }
 
 LoadingImage.blotName = "imageBlot";
