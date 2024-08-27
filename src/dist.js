d5ec9c2d6198ebb753096fe5b09121d88d6ebf87
csculley <csculley@csculley.com>
2024-08-27 15:12:37 -0400

    fix exports

--- src/dist.js
+++ src/dist.js
@@ -1,2 +1,3 @@
-export { default } from "./quill.imageUploader";
+export { default as ImageUploader } from "./quill.imageUploader";
+export { default as LoadingImage } from "./blots/loadingImage";
 import "./quill.imageUploader.css";
